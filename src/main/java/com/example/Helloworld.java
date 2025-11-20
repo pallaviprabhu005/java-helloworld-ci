8a6ff2961c9cb706444cedca6cb67e1ebc2fb94a
Pallavi Prabhu <pallaviprabhu05@gmail.com>
2025-11-20 06:55:15 +0530

    Update and rename helloworld.java to Helloworld.java

--- src/main/java/com/example/Helloworld.java
+++ src/main/java/com/example/Helloworld.java
@@ -4,4 +4,5 @@ public static void main(String[] args) {
 System.out.println("Hello, DevOps CI/CD using GitHub Actions!"); 
 System.out.println("Change #1: Verifying automatic workflow trigger on push."); 
 } 
+
 } 
